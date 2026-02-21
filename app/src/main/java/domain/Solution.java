d7f53d61c6bc08a2a3d7ef6ee936459f5103c3d7
gasham <gashammamedgasanov@gmail.com>
2026-02-21 15:11:23 +0300

    Initial commit

---
+++ app/src/main/java/domain/Solution.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class Solution {
+    private long id;
+    private String name;
+    private double correction;
+    private SolutionConcentrationUnit concentrationUnit;
+    private String solvent;
+    private String ownerUsername;
+    private String createdAt;
+    private String updatedAt;
+}
---
+++ app/src/main/java/domain/SolutionConcentrationUnit.java
@@ -0,0 +1,7 @@
+package domain;
+
+public enum SolutionConcentrationUnit {
+    PERCENT,
+    MOL_PER_L,
+    G_PER_L
+}
