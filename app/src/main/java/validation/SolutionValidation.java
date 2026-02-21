93eebb6c5295450ff39703487819fb6aa81852ee
gasham <gashammamedgasanov@gmail.com>
2026-02-21 18:07:42 +0300

    added SolutionValidation class

--- app/src/main/java/validation/SolutionValidation.java
+++ app/src/main/java/validation/SolutionValidation.java
@@ -1,11 +1,20 @@
 package validation;
 import domain.Solution;
-import org.gradle.internal.impldep.org.jsoup.helper.ValidationException;
+
 
 public class SolutionValidation {
-    public static void validate(Solution solution) {
+    public static void validate(Solution solution) throws IllegalAccessException {
         if (solution.getName()== null) {
-            throw new ValidationException("Name is required");
+            throw new IllegalAccessException("Название раствора не может быть пустым");
+        }
+        if (solution.getName().length() > 128) {
+            throw new IllegalAccessException("Название раствора сликом длинное(должно быть не более 128 символов)");
+        }
+        if (solution.getConcentration()<0){
+            throw new IllegalAccessException("Концентрация не должна быть отрицательной");
+        }
+        if (solution.getSolvent().length()>64){
+            throw new IllegalAccessException("Название растворителя слишком длинное(должно быть не более 64 символов)");
         }
     }
 }
