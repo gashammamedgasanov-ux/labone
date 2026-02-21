8294ec510a84e95a602415716bd2bc8b8b685729
gasham <gashammamedgasanov@gmail.com>
2026-02-21 20:30:41 +0300

    написал часть методов для SolutionManager

--- app/src/main/java/manger/SolutionManager.java
+++ app/src/main/java/manger/SolutionManager.java
@@ -1,7 +1,10 @@
 package manger;
 
 import domain.Solution;
+import domain.SolutionConcentrationUnit;
 import validation.SolutionValidation;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.time.Instant;
@@ -10,10 +13,55 @@ public class SolutionManager {
     private final Map<Long, Solution> solutions = new HashMap<>();
     private long nextId = 1;
 
-    public Solution addSolution(Solution solution) {
-        solution.setId(nextId++);
-        solutions.put(solution.getId(), solution);
-        return solution;
+    public Solution addSolution(String name, double concentration, SolutionConcentrationUnit concentrationUnit, String solvent, String ownerUsername) throws IllegalAccessException {
+       Solution solution = new Solution(name, concentration, concentrationUnit, solvent, ownerUsername);
+       SolutionValidation.validate(solution);
+       solution.setId(nextId++);
+       solution.setCreatedAt(Instant.now());
+       solution.setUpdatedAt(Instant.now());
+       solutions.put(solution.getId(), solution);
+       return solution;
     }
+
+    public Solution getSolution(Long id) {
+        if (solutions.get(id) == null) {
+            throw new IllegalArgumentException("Раствор с id=" + id + " не найден");
+        }
+        return solutions.get(id);
+    }
+
+    public Map<Long, Solution> getAll(){
+         return Collections.unmodifiableMap(solutions);
+    }
+
+    public void updateSolution(Long id, String newName, Double newConcentration, SolutionConcentrationUnit newUnit, String newSolvent) throws IllegalAccessException {
+        Solution existing=getSolution(id);
+        if (existing == null) {
+            throw new IllegalArgumentException("Раствор с таким id =" + id + " не сущeствует");
+        }
+        Solution draftSolution = new Solution(
+                newName != null ? newName : existing.getName(),
+                newConcentration != null ? newConcentration : existing.getConcentration(),
+                newUnit != null ? newUnit : existing.getConcentrationUnit(),
+                newSolvent != null ? newSolvent : existing.getSolvent(),
+                existing.getOwnerUsername()
+        );
+        SolutionValidation.validate(draftSolution);
+        if (newName != null) existing.setName(newName);
+        if (newConcentration != null) existing.setConcentration(newConcentration);
+        if (newUnit != null) existing.setConcentrationUnit(newUnit);
+        if (newSolvent != null) existing.setSolvent(newSolvent);
+        existing.setUpdatedAt(Instant.now());
+        solutions.put(id, draftSolution);
+
+    }
+
+    public void removeSolution(Long id) {
+        if (!solutions.containsKey(id)) {
+            throw new IllegalArgumentException("Раствор с id=" + id + " не найден");
+        }
+        solutions.remove(id);
+    }
+
 
 }
