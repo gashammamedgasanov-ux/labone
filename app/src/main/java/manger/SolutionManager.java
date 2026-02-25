4bcec6f0965899997824aec2cf34c114605b5dbc
gasham <gashammamedgasanov@gmail.com>
2026-02-25 18:43:57 +0300

    исправил метод updateSolution

--- app/src/main/java/manger/SolutionManager.java
+++ app/src/main/java/manger/SolutionManager.java
@@ -13,29 +13,31 @@ public class SolutionManager {
     private final Map<Long, Solution> solutions = new HashMap<>();
     private long nextId = 1;
 
+    //метод для команды sol_add
     public Solution addSolution(String name, double concentration, SolutionConcentrationUnit concentrationUnit, String solvent, String ownerUsername) throws IllegalAccessException {
-       Solution solution = new Solution(name, concentration, concentrationUnit, solvent, ownerUsername);
-       SolutionValidation.validate(solution);
-       solution.setId(nextId++);
-       solution.setCreatedAt(Instant.now());
-       solution.setUpdatedAt(Instant.now());
-       solutions.put(solution.getId(), solution);
-       return solution;
+        Solution solution = new Solution(name, concentration, concentrationUnit, solvent, ownerUsername);
+        SolutionValidation.validate(solution);
+        solution.setId(nextId++);
+        solution.setCreatedAt(Instant.now());
+        solution.setUpdatedAt(Instant.now());
+        solutions.put(solution.getId(), solution);
+        return solution;
     }
 
+    //метолд для команды sol_show
     public Solution getSolution(Long id) {
         if (solutions.get(id) == null) {
             throw new IllegalArgumentException("Раствор с id=" + id + " не найден");
         }
         return solutions.get(id);
     }
-
-    public Map<Long, Solution> getAll(){
-         return Collections.unmodifiableMap(solutions);
+// метод для команды sol_list
+    public Map<Long, Solution> getAll() {
+        return Collections.unmodifiableMap(solutions);
     }
 
     public void updateSolution(Long id, String newName, Double newConcentration, SolutionConcentrationUnit newUnit, String newSolvent) throws IllegalAccessException {
-        Solution existing=getSolution(id);
+        Solution existing = getSolution(id);
         if (existing == null) {
             throw new IllegalArgumentException("Раствор с таким id =" + id + " не сущeствует");
         }
@@ -44,16 +46,13 @@ public class SolutionManager {
                 newConcentration != null ? newConcentration : existing.getConcentration(),
                 newUnit != null ? newUnit : existing.getConcentrationUnit(),
                 newSolvent != null ? newSolvent : existing.getSolvent(),
-                existing.getOwnerUsername()
-        );
+                existing.getOwnerUsername());
         SolutionValidation.validate(draftSolution);
         if (newName != null) existing.setName(newName);
         if (newConcentration != null) existing.setConcentration(newConcentration);
         if (newUnit != null) existing.setConcentrationUnit(newUnit);
         if (newSolvent != null) existing.setSolvent(newSolvent);
         existing.setUpdatedAt(Instant.now());
-        solutions.put(id, draftSolution);
-
     }
 
     public void removeSolution(Long id) {
