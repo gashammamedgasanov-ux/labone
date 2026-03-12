72dd76137e48d2fc057f320841ada4257ca5e184
gasham <gashammamedgasanov@gmail.com>
2026-03-12 23:14:46 +0300

    исправленно соответсвенно комментарию

--- app/src/main/java/manager/BatchService.java
+++ app/src/main/java/manager/BatchService.java
@@ -1,8 +1,11 @@
 package manager;
 
 public class BatchService {
+    private static final int MAX_BATCH_ID = 100;
+    private static final int MIN_BATCH_ID = 1;
+
     public boolean exists(Long id) {
-        return id != null && id > 0 && id <= 100;
+        return id != null && id >= MIN_BATCH_ID && id <= MAX_BATCH_ID;
     }
 }
 //  не является частью нашей предметной области однако связан с компонентом приготовления, сделали временное решение
